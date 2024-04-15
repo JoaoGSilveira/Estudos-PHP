98927755cd5e6d867fe4eb9c859a47800567db37
JoaoGSilveira <joaosilveira31@hotmail.com>
2024-04-15 17:11:28 -0300

    Update

--- Udemy (PHP)/Carro.class.php
+++ Udemy (PHP)/Carro.class.php
@@ -1,15 +1,51 @@
 <?php
 class Carro{
-    var $nome, $cor;
+    private $ligado, $vAtual, $vMax, $msg;
 
-    public function validar($nome, $cor){
-        $this -> nome = $nome;
-        $this -> cor = $cor;
+    public function __construct($vm){
+        $this -> setLigado(false);
+        $this -> setVatual(0);
+        $this -> setVmax($vm);
     }
 
-    public function exibir(){
-        echo "Nome:" . $this -> nome . "<br>";
-        echo "Cor:" . $this -> cor . "<br>";
+    // Metodos Setters
+    public function setLigado($ligado){
+        return $this -> ligado = $ligado;
     }
+
+    public function setVatual($vAtual){
+        return $this -> vAtual = $vAtual;
+    }
+
+    public function setVmax($vMax){
+        return $this -> vMax = $vMax;
+    }
+
+    public function setMsg($msg){
+        return $this -> msg = $msg;
+    }
+
+    // Metodos Getters
+
+    public function getLigado()
+    {
+        return $this -> ligado
+    }
+
+    public function getVatual()
+    {
+        return $this->vAtual;
+    }
+
+    public function getVmax()
+    {
+        return $this->vMax;
+    }
+
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
 }
 ?>
---
+++ Udemy (PHP)/carro.php
@@ -0,0 +1,5 @@
+<?php
+    require_once 'Carro.class.php';
+
+    
+?>
